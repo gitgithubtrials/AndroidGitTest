55026ed3cae117f6f613a0bebe9402a43401b26b
user <humeyra.mercan@ogr.sakarya.edu.tr>
2022-06-09 15:32:10 +0300

    third line

--- app/src/main/java/com/humeyramercan/androidgittest/MainActivity.kt
+++ app/src/main/java/com/humeyramercan/androidgittest/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("hello")
         println("second line")
+        println("third line")
     }
 }
